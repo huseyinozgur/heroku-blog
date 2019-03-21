bd099240a2ba7f558f46f02034ddcfaa358bfd8f
Hüseyin Özgür <huseyin.ozgur@vektora.com>
2019-03-21 14:35:19 +0300

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace heroku_blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
